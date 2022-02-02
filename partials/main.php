1dd29bb3df074c3d792f4b278b146dd8dc4e5baa
Stefano Ruggiu <steruggiu@gmail.com>
2022-02-02 14:58:11 +0000

    importato bootstrap e copiata struttura main in main.php

---
+++ partials/main.php
@@ -0,0 +1,12 @@
+<div class="container">
+      <div class="row mb-4">
+        <div class="search d-flex justify-content-center">
+        </div>
+      </div>
+      <div class="row cards">
+        <div class="col-2 card mx-3 my-2">
+
+        </div>
+        
+      </div>
+    </div>
