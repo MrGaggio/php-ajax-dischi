204d82bc556d3ea83a137b7a1579b0b5aaee0f8c
Stefano Ruggiu <steruggiu@gmail.com>
2022-02-02 16:26:16 +0000

    corretto percorso(grazie Thomas) e add cards

--- partials/cards.php
+++ partials/cards.php
@@ -1,13 +1,17 @@
-<?php include_once __DIR__ . '../server/db.php' ?>
+<?php include_once __DIR__ . '/../server/db.php' ?>
 
 <div class="col-2 card mx-3 my-2">
     <?php foreach ($cards as $card) { ?>
-        <img src="" alt="">
+        <img src="<?php echo ($card['poster']) ?>" alt="">
         <h2 class="f2-bold">
             <?php echo ($card['title']) ?>
         </h2>
-        <h5 class="fs-5 my-0"></h5>
-        <h6 class="fs-6"></h6>
+        <h5 class="fs-5 my-0">
+        <?php echo ($card['author']) ?>
+        </h5>
+        <h6 class="fs-6">
+        <?php echo ($card['year']) ?>
+        </h6>
    <?php } ?>
 
 
