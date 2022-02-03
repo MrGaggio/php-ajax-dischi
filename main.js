638d94955abd1cf63b5976ac486bb9f1f3f65a26
Stefano Ruggiu <steruggiu@gmail.com>
2022-02-03 00:08:53 +0000

    installato axios  e inserito link chiamata

--- main.js
+++ main.js
@@ -2,11 +2,20 @@
 var app = new Vue({
     el: '#app',
     data: {
-     
+    
     },
     created: {
-      
+      axios:get('https://flynn.boolean.careers/exercises/api/array/music')
+  .then(function (response) {
+    // handle success
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
     }
+    
   })
 
 
